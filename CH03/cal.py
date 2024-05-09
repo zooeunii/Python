43f2bad62054f432d9126825c1126309f6abc20b
주은 <168528163+zooeunii@users.noreply.github.com>
2024-05-10 01:27:20 +0900

    Add files via upload

---
+++ CH03/cal.py
@@ -0,0 +1,15 @@
+i = 5
+j = 7
+s = '파이썬'
+print(i + j)
+print(i + 2)
+print('안녕' + s)
+
+i = i - j
+print(i)
+
+s = s * 3
+print(s)
+
+s = 'Python'
+print(s)
---
+++ CH03/pri.py
@@ -0,0 +1,22 @@
+a = '홍길동'
+a
+print(a)
+
+b = '27'
+b
+print(b)
+
+c = 27
+c
+print(c)
+
+d = '첫째 줄\n둘째 줄'
+d
+print(d)
+
+'Python',1,2,        3
+print('Python',1,2,     3)
+
+import sys
+sys
+print(sys)
