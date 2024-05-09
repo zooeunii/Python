2eeeb14a6cb30b9d5ed335538ae53d604cfd20e0
주은 <168528163+zooeunii@users.noreply.github.com>
2024-05-10 01:49:01 +0900

    06. input.py

--- 바로 쓰는 파이썬/CH03/06. input.py
+++ 바로 쓰는 파이썬/CH03/06. input.py
@@ -1,7 +1,7 @@
 name = input('이름을 입력하세요: ')     #input() 함수를 이용해 변수에 값을 할당한다.
-print(name)     #변수 name을 출력한다.
-name        #문자열
+print(name)                            #변수 name을 출력한다.
+name                                   #문자열
 
 age = input('나이를 입력하세요: ')      #input() 함수를 이용해 변수에 값을 할당한다.
-print(age)      #변수 age을 출력한다.
-print(int(age) + 1)     #int() 생성자를 사용해서 문자열을 정수로 변환한 후 1을 더한다.
+print(age)                            #변수 age을 출력한다.
+print(int(age) + 1)                   #int() 생성자를 사용해서 문자열을 정수로 변환한 후 1을 더한다.
