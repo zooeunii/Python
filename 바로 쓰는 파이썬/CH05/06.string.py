1ec2319e4c4140204ab057016d2a300fdc221019
주은 <168528163+zooeunii@users.noreply.github.com>
2024-05-15 22:41:05 +0900

    Add files via upload

---
+++ 바로 쓰는 파이썬/CH05/06.string.py
@@ -0,0 +1,10 @@
+#문자열 리터럴 만들기
+#print() 함수를 한 번만 사용해서 다음 텍스트 출력
+#"She's gone!Yon'sHeathcliff's run off wi'her!" gasped the girl.
+#"That is not true!" exclaimed Linton, rising in agitation.
+#변수 text에 위의 문장을 문자열로 할당한다.
+#변수 text를 출력한다.
+
+text = '''"She's gone!Yon'sHeathcliff's run off wi'her!" gasped the girl.
+"That is not true!" exclaimed Linton, rising in agitation.'''
+print(text)
